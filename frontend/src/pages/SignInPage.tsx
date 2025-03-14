592d7f607ad3dcc28dc579a6a7355102be0ca4c6
cea <me@danicea.com>
2025-03-14 01:02:49 +0100

    - Fix vite problem - Add tailwind and signup form component + page

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -1,12 +1,26 @@
 import { SignInForm } from "../components";
 
 function SignInPage() {
-    return (
-        <div>
-        <h1 className="text-3xxl font-bold underline">Sign In</h1>
-        <SignInForm />
-        </div>
-    );
+
+  const handleSignIn = (formData: FormData) => {
+    debugger;
+    console.log(formData.get("email"))
+    console.log(formData.get("password"))
+    console.log(formData.get("confirm-password"))
+  }
+
+  return (
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+               alt="logo"/>
+          Flowbite
+        </a>
+        <SignInForm handleFormSubmit={handleSignIn}/>
+      </div>
+    </section>
+  );
 }
 
 export default SignInPage;
