001c6944742c10acd4a5d2e008b0283ee542591e
cea <me@danicea.com>
2025-03-21 19:22:22 +0100

    - Serialize user correctly

--- backend/app/controllers/auth_controller.rb
+++ backend/app/controllers/auth_controller.rb
@@ -8,14 +8,9 @@ class AuthController < ApplicationController
 
     if user.save
       token = encode_token({ user_id: user.id, tenant_id: user.tenant_id, exp: 1.year.from_now.to_i })
-      cookies[:_auth] = {
-        value: token,
-        secure: true,  # Only send over HTTPS in production
-        same_site: :none,  # Allow cross-site cookies
-        expires: 1.week.from_now # Set an expiration time for the cookie
-      }
+      set_auth_cookie(token)
 
-      render json: { token: token, user: user }, status: :created
+      render json: { token: token, user: UserSerializer.new(user).serializable_hash }, status: :created
     else
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
@@ -26,17 +21,11 @@ class AuthController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       token = encode_token({ user_id: user.id, tenant_id: user.tenant_id, exp: 1.year.from_now.to_i })
+      set_auth_cookie(token)
 
-      cookies[:_auth] = {
-        value: token,
-        secure: true,  # Only send over HTTPS in production
-        same_site: :none,  # Allow cross-site cookies
-        expires: 1.week.from_now # Set an expiration time for the cookie
-      }
-
-      render json: { token: token, user: user }, status: :ok
+      render json: { token: token, user: UserSerializer.new(user).serializable_hash }, status: :ok
     else
-      render json: { errors: ["Invalid email or password"] }, status: :unauthorized
+      render json: { errors: [ "Invalid email or password" ] }, status: :unauthorized
     end
   end
 
@@ -47,6 +36,15 @@ class AuthController < ApplicationController
   end
 
   def encode_token(payload)
-    JWT.encode(payload, Rails.application.secret_key_base, 'HS256')
+    JWT.encode(payload, Rails.application.secret_key_base, "HS256")
+  end
+
+  def set_auth_cookie(token)
+    cookies[:_auth] = {
+      value: token,
+      secure: true,  # Only send over HTTPS in production
+      same_site: :none,  # Allow cross-site cookies
+      expires: 1.week.from_now # Set an expiration time for the cookie
+    }
   end
 end
--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :role
+  attributes :name, :email, :role
 end
